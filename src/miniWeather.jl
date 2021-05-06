cd12d6b2e1ac5f7f9906cf48828f1f9d6affba68
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2021-05-06 20:51:12 +0200

    include also utils

--- src/miniWeather.jl
+++ src/miniWeather.jl
@@ -7,9 +7,10 @@ import Base.Threads.@spawn
 #using LoopVectorization: @avx
 
 include("const.jl")
+include("utils.jl")
 include("Initialize.jl")
 include("Timestep.jl")
 
-export Grid, Model, init, run, reductions, DATA_SPEC_THERMAL
+export Grid, Model, init, simulate, reductions, DATA_SPEC_THERMAL
 
 end
